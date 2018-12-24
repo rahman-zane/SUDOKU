f88491546cf2e2d6854a7264967a6dbf631eb8b6
irej98 <mojeri.oyenuga@gmail.com>
2018-12-24 23:52:47 +0000

    Creation of initial Ideas and Drafts. 24/12/2018 23:50  - Created file for initial input ideas for Sudoku Grid

---
+++ Jeri/IdeasAndDrafts/InputFormat.py
@@ -0,0 +1,7 @@
+# Created on 24/12/2018 23:47 by Jeri
+
+# Initial ideas for input format.
+# Research suggests for smaller arrays, numpy is ideal, so will use numpy arrays as input format for Sudoku grid.
+
+# Import required modules
+import numpy as np
